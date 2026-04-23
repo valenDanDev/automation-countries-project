fcd34c07b0312a8a873d9b6303ad41749b0bda5e
valenDanDev <valenciadaniel8022015@gmail.com>
2026-04-22 20:59:49 -0500

    cypress configuration for reports

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,16 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   allowCypressEnv: false,
 
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: false,
+    json: true
+  },
+
   e2e: {
-     baseUrl: "http://localhost:3000",
+    baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
       // future: plugins, logs, tasks
     },
